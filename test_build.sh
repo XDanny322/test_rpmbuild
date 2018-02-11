f048906737719de59dd4ac9b7ec5ed10d61a50d6
Danny Lai <dlai@portware.com>
2018-02-11 18:54:33 -0500

    Removed unneeded Vars from sh script

--- test_build.sh
+++ test_build.sh
@@ -8,12 +8,8 @@ set -e
 ################################################################################
 # Build variables
 SALT_VERSION=${SALT_VERSION:-'2017.7.2'}
-AUTOMATION_VERSION=${AUTOMATION_VERSION:-'12'}
-FULL_VERSION="$SALT_VERSION.$AUTOMATION_VERSION"
 BUILD_NUMBER=${BUILD_NUMBER:-'SNAPSHOT'}
-SALT_MASTER=${SALT_MASTER:-'localhost'}
-RPM_NAME=${RPM_NAME:-'fds-auto-salt-minion'}
-PIP_VERSION=${PIP_VERSION:-'9.0.1'}
+RPM_NAME=${RPM_NAME:-'test_rpm'}
 
 ################################################################################
 # Debug / information gathering
@@ -43,10 +39,4 @@ pwd
 rpmbuild \
   --define "_topdir $WORKSPACE"  \
   --define "name $RPM_NAME"  \
-  --define "salt_version $SALT_VERSION"  \
-  --define "version $FULL_VERSION"  \
-  --define "release $BUILD_NUMBER%{?dist}"  \
-  --define "salt_minion_config minion.conf"  \
-  --define "salt_minion_service salt-minion.service.systemd"  \
-  --define "logrotate_config salt.logrotate"  \
-  --define "pip_version $PIP_VERSION" -ba -vv test_rpm.spec
+  --define "salt_version $SALT_VERSION" -ba -vv test_rpm.spec
